73d3b758378296829ef912dedd5fee936296b7d2
einfachIrgendwer0815 <eavmb@gmx.de>
2021-08-18 16:44:51 +0200

    autogeneration of voice channels for multiple categories

--- modules/cogs/voiceChannel.py
+++ modules/cogs/voiceChannel.py
@@ -22,10 +22,11 @@ class VoiceChannel(commands.Cog):
     async def initialAutoGenCheck(self):
         for guildId in self.moduleStates.loadedGuilds.keys():
             if self.moduleStates.isLoaded('autoGenChannel', guildId):
-                category = functions.get_channel(int(guildId), self.configs.autoGenChannel[guildId]["category"], self.bot)
+                for categoryId in self.configs.autoGenChannel[guildId]["categories"]:
+                    category = functions.get_channel(int(guildId), categoryId, self.bot)
 
-                if len(category.voice_channels) == 0:
-                    await category.create_voice_channel("Channel")
+                    if len(category.voice_channels) == 0:
+                        await category.create_voice_channel("Channel")
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -38,16 +39,16 @@ class VoiceChannel(commands.Cog):
         if action == 0:
             return
 
-        category, doCheck = await self.findAutoGenChannelInfos(guildId, before, after)
+        doCheck = await self.findAutoGenChannelInfos(guildId, before, after)
 
         if action == 1:
-            await self.on_join(guildId, member, after, category, doCheck)
+            await self.on_join(guildId, member, after, doCheck)
 
         elif action == 2:
-            await self.on_move(guildId, member, before, after, category, doCheck)
+            await self.on_move(guildId, member, before, after, doCheck)
 
         elif action == 3:
-            await self.on_leave(guildId, member, before, category, doCheck)
+            await self.on_leave(guildId, member, before, doCheck)
 
     async def findAction(self, before, after):
         action = 0 # 0 = default; 1 = join; 2 = move; 3 = leave
@@ -58,45 +59,50 @@ class VoiceChannel(commands.Cog):
         return action
 
     async def findAutoGenChannelInfos(self, guildId, before, after):
+        doCheck = None
         if self.moduleStates.isLoaded('autoGenChannel', guildId):
-            category = functions.get_channel(guildId, self.configs.autoGenChannel[str(guildId)]["category"], self.bot)
-            doCheck = True if before.channel in category.voice_channels or after.channel in category.voice_channels else False
-        else:
-            category = None
-            doCheck = None
+            for categoryId in self.configs.autoGenChannel[str(guildId)]['categories']:
+                category = functions.get_channel(guildId, categoryId, self.bot)
+                doCheck = True if before.channel in category.voice_channels or after.channel in category.voice_channels else doCheck
 
-        return (category, doCheck)
+        return doCheck
 
-    async def on_join(self, guildId, member, after, category, doCheck):
+    async def on_join(self, guildId, member, after, doCheck):
         if self.moduleStates.isLoaded('logJoinLeaveChannel', guildId):
             self.voiceChannelLogger[str(guildId)].logJoin(member.name, member.id, after.channel.name, after.channel.id)
 
-        await self.on_join_(guildId, category, doCheck)
+        await self.on_join_(guildId, doCheck)
 
-    async def on_join_(self, guildId, category, doCheck):
+    async def on_join_(self, guildId, doCheck):
         if self.moduleStates.isLoaded('autoGenChannel', guildId) and doCheck:
-            if functions.activeVoiceChannels(category.voice_channels) == len(category.voice_channels):
-                await category.create_voice_channel("Channel")
+            for categoryId in self.configs.autoGenChannel[str(guildId)]['categories']:
+                category = functions.get_channel(guildId, categoryId, self.bot)
 
-    async def on_leave(self, guildId, member, before, category, doCheck):
+                if functions.activeVoiceChannels(category.voice_channels) == len(category.voice_channels):
+                    await category.create_voice_channel("Channel")
+
+    async def on_leave(self, guildId, member, before, doCheck):
         if self.moduleStates.isLoaded('logJoinLeaveChannel', guildId):
             self.voiceChannelLogger[str(guildId)].logLeave(member.name, member.id, before.channel.name, before.channel.id)
 
-        await self.on_leave_(guildId, category, doCheck)
+        await self.on_leave_(guildId, doCheck)
 
 
-    async def on_leave_(self, guildId, category, doCheck):
+    async def on_leave_(self, guildId, doCheck):
         if self.moduleStates.isLoaded('autoGenChannel', guildId) and doCheck:
-            for channel in category.voice_channels:
-                if functions.activeVoiceChannels(category.voice_channels) < len(category.voice_channels)-1:
-                    if len(channel.members) == 0:
-                        await channel.delete()
-                else:
-                    break
+            for categoryId in self.configs.autoGenChannel[str(guildId)]['categories']:
+                category = functions.get_channel(guildId, categoryId, self.bot)
 
-    async def on_move(self, guildId, member, before, after, category, doCheck):
+                for channel in category.voice_channels:
+                    if functions.activeVoiceChannels(category.voice_channels) < len(category.voice_channels)-1:
+                        if len(channel.members) == 0:
+                            await channel.delete()
+                    else:
+                        break
+
+    async def on_move(self, guildId, member, before, after, doCheck):
         if self.moduleStates.isLoaded('logJoinLeaveChannel', guildId):
             self.voiceChannelLogger[str(guildId)].logMove(member.name, member.id, before.channel.name, before.channel.id, after.channel.name, after.channel.id)
 
-        await self.on_join_(guildId, category, doCheck)
-        await self.on_leave_(guildId, category, doCheck)
+        await self.on_join_(guildId, doCheck)
+        await self.on_leave_(guildId, doCheck)
